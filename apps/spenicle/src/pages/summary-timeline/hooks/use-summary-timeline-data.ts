ab6feaa872749b7ecedd6b2113a22836fec3fa0a
dimasbaguspm <dimas.bagus.pm1@gmail.com>
2025-08-30 04:32:20 +0700

    feat(summary-timeline): implement summary timeline card and data hook

---
+++ apps/spenicle/src/pages/summary-timeline/hooks/use-summary-timeline-data.ts
@@ -0,0 +1,85 @@
+import {
+  useApiSpenicleAccountsPaginatedQuery,
+  useApiSpenicleCachedAccounts,
+  useApiSpenicleCachedCategories,
+  useApiSpenicleCategoriesPaginatedQuery,
+  useApiSpenicleTransactionsInfiniteQuery,
+} from '@dimasbaguspm/hooks/use-api';
+
+import { useSummaryFilter } from '../../summary/hooks/use-summary-filter';
+
+export const useSummaryTimelineData = () => {
+  const { appliedFilters } = useSummaryFilter();
+
+  const dateFilters = {
+    from: appliedFilters.range.startDate.toISOString(),
+    to: appliedFilters.range.endDate.toISOString(),
+    categoryIds: appliedFilters.categoryIds,
+    accountIds: appliedFilters.accountIds,
+  };
+
+  const [
+    transactions,
+    ,
+    { hasNextPage, isFetching, isFetchingNextPage },
+    { fetchNextPage },
+  ] = useApiSpenicleTransactionsInfiniteQuery({
+    dateFrom: dateFilters.from,
+    dateTo: dateFilters.to,
+    categoryId: appliedFilters.categoryIds,
+    accountId: appliedFilters.accountIds,
+    pageSize: 15,
+    sortBy: 'date',
+  });
+
+  const cachedAccounts = useApiSpenicleCachedAccounts();
+  const needToFetchAccounts = transactions
+    .map((t) => t.accountId)
+    .filter((id) => id && !cachedAccounts.some((ca) => ca.id === id));
+
+  const cachedCategories = useApiSpenicleCachedCategories();
+  const needToFetchCategories = transactions
+    .map((t) => t.categoryId)
+    .filter((id) => id && !cachedCategories.some((cc) => cc.id === id));
+
+  useApiSpenicleAccountsPaginatedQuery(
+    {
+      id: needToFetchAccounts,
+      pageSize: 15,
+    },
+    {
+      enabled: Boolean(needToFetchAccounts.length),
+    },
+  );
+  useApiSpenicleCategoriesPaginatedQuery(
+    {
+      id: needToFetchCategories,
+      pageSize: 15,
+    },
+    {
+      enabled: Boolean(needToFetchCategories.length),
+    },
+  );
+
+  const data = (transactions || []).map((transaction) => {
+    const account = cachedAccounts.find((a) => a.id === transaction.accountId)!;
+    const category = cachedCategories.find(
+      (c) => c.id === transaction.categoryId,
+    )!;
+    return {
+      transaction,
+      account,
+      category,
+    };
+  });
+
+  const isInitialLoading = isFetching && !isFetchingNextPage;
+
+  return {
+    data,
+    isInitialLoading,
+    isFetchingNextPage,
+    hasNextPage,
+    fetchNextPage,
+  };
+};
--- apps/spenicle/src/pages/summary/components/action-header.tsx
+++ apps/spenicle/src/pages/summary/components/action-header.tsx
@@ -69,13 +69,13 @@ export const ActionHeader: FC = () => {
           <Icon as={TargetIcon} color="inherit" size="sm" />
           {isDesktop && 'Overview'}
         </ChipSingleInput.Option>
-        <ChipSingleInput.Option value="calendar">
-          <Icon as={CalendarCheck2Icon} color="inherit" size="sm" />
-          {isDesktop && 'Calendar'}
-        </ChipSingleInput.Option>
         <ChipSingleInput.Option value="timeline">
           <Icon as={ChartBarBigIcon} color="inherit" size="sm" />
           {isDesktop && 'Timeline'}
+        </ChipSingleInput.Option>
+        <ChipSingleInput.Option value="calendar">
+          <Icon as={CalendarCheck2Icon} color="inherit" size="sm" />
+          {isDesktop && 'Calendar'}
         </ChipSingleInput.Option>
       </ChipSingleInput>
     </div>
