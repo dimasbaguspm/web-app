9bc11305f7bf277103c79480ed115771add71151
dimasbaguspm <dimas.bagus.pm1@gmail.com>
2025-08-30 10:27:26 +0700

    feat(detail-transaction-drawer): enhance transaction details with account and category info

--- apps/spenicle/src/drawers/detail-transaction-drawer/drawer.tsx
+++ apps/spenicle/src/drawers/detail-transaction-drawer/drawer.tsx
@@ -1,6 +1,15 @@
-import { useApiSpenicleTransactionQuery } from '@dimasbaguspm/hooks/use-api';
+import {
+  useApiSpenicleAccountQuery,
+  useApiSpenicleCategoryQuery,
+  useApiSpenicleTransactionQuery,
+} from '@dimasbaguspm/hooks/use-api';
 import { useDrawerRoute } from '@dimasbaguspm/providers/drawer-route-provider';
-import { formatSpenicleTransaction } from '@dimasbaguspm/utils/data';
+import {
+  formatSpenicleAccount,
+  formatSpenicleCategory,
+  formatSpenicleTransaction,
+} from '@dimasbaguspm/utils/data';
+import { DateFormat, formatDate } from '@dimasbaguspm/utils/date';
 import { If } from '@dimasbaguspm/utils/if';
 import {
   AttributeList,
@@ -8,12 +17,14 @@ import {
   BadgeGroup,
   Button,
   ButtonGroup,
+  ButtonIcon,
   Drawer,
   Icon,
   LoadingIndicator,
   Text,
 } from '@dimasbaguspm/versaur';
-import { Edit2Icon } from 'lucide-react';
+import dayjs from 'dayjs';
+import { CopyIcon, Edit2Icon, TrashIcon } from 'lucide-react';
 import { FC } from 'react';
 
 import { DRAWER_ROUTES } from '../../constants/drawer-routes';
@@ -25,12 +36,34 @@ interface DetailTransactionDrawerProps {
 export const DetailTransactionDrawer: FC<DetailTransactionDrawerProps> = ({
   transactionId,
 }) => {
-  const [transactionData, , { isFetching }] =
-    useApiSpenicleTransactionQuery(transactionId);
   const { openDrawer } = useDrawerRoute();
 
-  const { note, variant, capitalizedType, time, date, amount, updatedAt } =
+  const [transactionData, , { isFetching }] =
+    useApiSpenicleTransactionQuery(transactionId);
+  const [accountData, , { isFetching: isFetchingAccount }] =
+    useApiSpenicleAccountQuery(transactionData?.accountId || 0, {
+      enabled: !!transactionData?.accountId,
+    });
+  const [
+    destinationAccountData,
+    ,
+    { isFetching: isFetchingDestinationAccount },
+  ] = useApiSpenicleAccountQuery(transactionData?.destinationAccountId || 0, {
+    enabled: !!transactionData?.destinationAccountId,
+  });
+  const [categoryData, , { isFetching: isFetchingCategory }] =
+    useApiSpenicleCategoryQuery(transactionData?.categoryId || 0, {
+      enabled: !!transactionData?.categoryId,
+    });
+
+  const { note, variant, capitalizedType, time, date, amount } =
     formatSpenicleTransaction(transactionData);
+
+  const { name: accountName } = formatSpenicleAccount(accountData);
+  const { name: destinationAccountName } = formatSpenicleAccount(
+    destinationAccountData,
+  );
+  const { name: categoryName } = formatSpenicleCategory(categoryData);
 
   const handleOnEditClick = () => {
     openDrawer(DRAWER_ROUTES.EDIT_TRANSACTION, {
@@ -38,24 +71,73 @@ export const DetailTransactionDrawer: FC<DetailTransactionDrawerProps> = ({
     });
   };
 
+  const handleOnCopyClick = () => {
+    openDrawer(DRAWER_ROUTES.NEW_TRANSACTION, undefined, {
+      state: {
+        payload: {
+          type: transactionData?.type,
+          date: formatDate(dayjs(), DateFormat.ISO_DATE),
+          time: formatDate(dayjs(), DateFormat.TIME_24H),
+          accountId:
+            typeof transactionData?.accountId === 'number'
+              ? transactionData?.accountId
+              : undefined,
+          destinationAccountId:
+            typeof transactionData?.destinationAccountId === 'number'
+              ? transactionData?.destinationAccountId
+              : undefined,
+          categoryId:
+            typeof transactionData?.categoryId === 'number'
+              ? transactionData?.categoryId
+              : undefined,
+          amount:
+            typeof transactionData?.amount === 'number'
+              ? transactionData?.amount
+              : undefined,
+          notes: transactionData?.note,
+        },
+      },
+    });
+  };
+
+  const isLoading =
+    isFetching ||
+    isFetchingAccount ||
+    isFetchingDestinationAccount ||
+    isFetchingCategory;
+
   return (
     <>
       <Drawer.Header>
         <Drawer.Title>Transaction Details</Drawer.Title>
         <Drawer.CloseButton />
       </Drawer.Header>
-      <If condition={[isFetching, !transactionData]}>
+      <If condition={[isLoading, !transactionData]}>
         <LoadingIndicator size="sm" type="bar" />
       </If>
 
-      <If condition={[!isFetching, transactionData]}>
+      <If condition={[!isLoading, transactionData]}>
         <Drawer.Body>
           <ButtonGroup className="mb-4">
             <Button variant="outline" onClick={handleOnEditClick}>
               <Icon as={Edit2Icon} size="sm" color="inherit" />
               Edit
             </Button>
+
+            <Button variant="outline" onClick={handleOnCopyClick}>
+              <Icon as={CopyIcon} size="sm" color="inherit" />
+              Copy
+            </Button>
+
+            <ButtonIcon
+              as={TrashIcon}
+              size="sm"
+              className="ml-auto"
+              variant="outline"
+              aria-label="Delete transaction"
+            />
           </ButtonGroup>
+
           <BadgeGroup className="mb-4">
             <Badge color={variant}>{capitalizedType}</Badge>
           </BadgeGroup>
@@ -70,9 +152,17 @@ export const DetailTransactionDrawer: FC<DetailTransactionDrawerProps> = ({
             <AttributeList.Item title="Amount">
               <Text>{amount}</Text>
             </AttributeList.Item>
-            <AttributeList.Item title="Last update">
-              <Text>{updatedAt}</Text>
+            <AttributeList.Item title="Category">
+              <Text>{categoryName}</Text>
             </AttributeList.Item>
+            <AttributeList.Item title="Source">
+              <Text>{accountName}</Text>
+            </AttributeList.Item>
+            <If condition={[destinationAccountName]}>
+              <AttributeList.Item title="Destination">
+                <Text>{destinationAccountName}</Text>
+              </AttributeList.Item>
+            </If>
             <If condition={[note]}>
               <AttributeList.Item title="Notes" span={2}>
                 <Text>{note}</Text>
