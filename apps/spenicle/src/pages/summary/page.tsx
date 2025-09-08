a87bbfcd6f80c788da1fdd9d300393493a39cab8
dimasbaguspm <dimas.bagus.pm1@gmail.com>
2025-09-08 16:39:30 +0700

    feat(summary): integrate snapdom for enhanced print functionality

--- apps/spenicle/src/pages/summary/page.tsx
+++ apps/spenicle/src/pages/summary/page.tsx
@@ -1,18 +1,37 @@
 import { Button, ButtonGroup, ButtonIcon, Icon, PageContent, PageHeader } from '@dimasbaguspm/versaur';
+import { snapdom } from '@zumer/snapdom';
 import { PrinterIcon } from 'lucide-react';
+import { useRef } from 'react';
 import { Outlet } from 'react-router';
 
 import { ActionHeader } from './components/action-header';
 
 const SummaryLayout = () => {
+  const ref = useRef<HTMLDivElement>(null);
+  const handleOnPrintClick = async () => {
+    if (!ref.current) return;
+
+    const result = await snapdom(ref.current);
+
+    const img = await result.toPng();
+    document.body.appendChild(img);
+
+    await result.download({
+      format: 'png',
+      compress: false,
+      filename: 'spenicle-exported-summary-' + new Date().toISOString(),
+      backgroundColor: '#ffffff',
+    });
+  };
+
   return (
-    <>
+    <div ref={ref}>
       <PageHeader
         title="Summary"
         subtitle="Manage your summary transactions"
         actions={
           <ButtonGroup>
-            <Button>
+            <Button onClick={handleOnPrintClick}>
               <Icon as={PrinterIcon} color="inherit" size="sm" />
               Report
             </Button>
@@ -20,7 +39,7 @@ const SummaryLayout = () => {
         }
         mobileActions={
           <ButtonGroup>
-            <ButtonIcon as={PrinterIcon} aria-label="Export" />
+            <ButtonIcon onClick={handleOnPrintClick} as={PrinterIcon} aria-label="Report" />
           </ButtonGroup>
         }
       />
@@ -28,7 +47,7 @@ const SummaryLayout = () => {
         <ActionHeader />
         <Outlet />
       </PageContent>
-    </>
+    </div>
   );
 };
 
